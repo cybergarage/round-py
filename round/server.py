f1ea00f838d9a381cc2ba26060ff774df3bafbb4
Satoshi Konno <skonno@cybergarage.org>
2016-01-13 19:51:18 +0900

    * Added initial codes.

---
+++ round/server.py
@@ -0,0 +1,12 @@
+#################################################################
+#
+# Round for Python
+#
+# Copyright (C) Satoshi Konno 2016
+#
+# This is licensed under BSD-style license, see file COPYING.
+#
+##################################################################
+
+class Server:
+    def __init__():
---
+++ tests/unit/server_test.py
@@ -0,0 +1,11 @@
+#################################################################
+#
+# Round for Python
+#
+# Copyright (C) Satoshi Konno 2016
+#
+# This is licensed under BSD-style license, see file COPYING.
+#
+##################################################################
+
+import pytest
